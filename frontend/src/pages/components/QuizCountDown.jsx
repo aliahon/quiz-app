955be17a09063725aa8fd5562892ef5ef8c0b3db
Nohaila EL IDRISSI <nohaila09el@gmail.com>
2024-08-22 16:36:15 +0100

    I'm not a designer, update the design of the timers

--- frontend/src/pages/components/QuizCountDown.jsx
+++ frontend/src/pages/components/QuizCountDown.jsx
@@ -40,30 +40,29 @@ export default function QuizCountDown({ session }) {
   ) {
     return (
       <div className="flex gap-2 items-center justify-center">
-        <span className="text-[#FBFAF8] text-[20px] sm:text-[40px]">:</span>
         <div className="flex gap-1 shadow-lg">
-          <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[20px] sm:text-[40px] py-1 px-2  rounded drop-shadow-xl">
+          <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[8px] sm:text-[16px] py-1 px-2  rounded drop-shadow-xl">
             0
           </span>
-          <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[20px] sm:text-[40px] py-1 px-2  rounded drop-shadow-xl">
-            0
-          </span>
-        </div>
-        <span className="text-[#FBFAF8] text-[20px] sm:text-[40px]">:</span>
-        <div className="flex gap-1">
-          <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[20px] sm:text-[40px] py-1 px-2  rounded drop-shadow-xl">
-            0
-          </span>
-          <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[20px] sm:text-[40px] py-1 px-2  rounded drop-shadow-xl">
+          <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[8px] sm:text-[16px] py-1 px-2  rounded drop-shadow-xl">
             0
           </span>
         </div>
-        <span className="text-[#FBFAF8] text-[20px] sm:text-[40px]">:</span>
+        <span className="text-[#FBFAF8] text-[8px] sm:text-[16px]">:</span>
         <div className="flex gap-1">
-          <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[20px] sm:text-[40px] py-1 px-2  rounded drop-shadow-xl">
+          <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[8px] sm:text-[16px] py-1 px-2  rounded drop-shadow-xl">
             0
           </span>
-          <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[20px] sm:text-[40px] py-1 px-2  rounded drop-shadow-xl">
+          <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[8px] sm:text-[16px] py-1 px-2  rounded drop-shadow-xl">
+            0
+          </span>
+        </div>
+        <span className="text-[#FBFAF8] text-[8px] sm:text-[16px]">:</span>
+        <div className="flex gap-1">
+          <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[8px] sm:text-[16px] py-1 px-2  rounded drop-shadow-xl">
+            0
+          </span>
+          <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[8px] sm:text-[16px] py-1 px-2  rounded drop-shadow-xl">
             0
           </span>
         </div>
@@ -74,11 +73,11 @@ export default function QuizCountDown({ session }) {
   return (
     <div className="flex gap-2 items-center justify-center">
       <div className="flex gap-1 shadow-lg">
-        <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[20px] sm:text-[16px] py-1 px-2  rounded drop-shadow-xl">
+        <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[8px] sm:text-[16px] py-1 px-2  rounded drop-shadow-xl">
           {remainingEndHours > 10 ? remainingEndSeconds.toString().charAt(0) : "0"}
         </span>
-        <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[20px] sm:text-[16px] py-1 px-2  rounded drop-shadow-xl">
-          {remainingEndHours > 10 ? remainingEndHours.toString().charAt(1): remainingEndHours.toString().charAt(0)}
+        <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[8px] sm:text-[16px] py-1 px-2  rounded drop-shadow-xl">
+          {remainingEndHours > 10 ? remainingEndHours.toString().charAt(1) : remainingEndHours.toString().charAt(0)}
         </span>
       </div>
       <span className="text-[#FBFAF8] text-[8px] sm:text-[16px]">:</span>
@@ -87,7 +86,7 @@ export default function QuizCountDown({ session }) {
           {remainingEndMinutes > 10 ? remainingEndSeconds.toString().charAt(0) : "0"}
         </span>
         <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[8px] sm:text-[16px] py-1 px-2  rounded drop-shadow-xl">
-          {remainingEndMinutes > 10 ? remainingEndMinutes.toString().charAt(1): remainingEndMinutes.toString().charAt(0)}
+          {remainingEndMinutes > 10 ? remainingEndMinutes.toString().charAt(1) : remainingEndMinutes.toString().charAt(0)}
         </span>
       </div>
       <span className="text-[#FBFAF8] text-[8px] sm:text-[16px]">:</span>
@@ -96,7 +95,7 @@ export default function QuizCountDown({ session }) {
           {remainingEndSeconds > 10 ? remainingEndSeconds.toString().charAt(0) : "0"}
         </span>
         <span className="bg-quiz-theme  font-semibold text-[#FBFAF8] text-[8px] sm:text-[16px] py-1 px-2  rounded drop-shadow-xl">
-          {remainingEndSeconds > 10 ? remainingEndSeconds.toString().charAt(1): remainingEndSeconds.toString().charAt(0)}
+          {remainingEndSeconds > 10 ? remainingEndSeconds.toString().charAt(1) : remainingEndSeconds.toString().charAt(0)}
         </span>
       </div>
     </div>
--- frontend/src/pages/components/SessionNotStarted.jsx
+++ frontend/src/pages/components/SessionNotStarted.jsx
@@ -30,68 +30,60 @@ export default function SessionNotStarted({ session }) {
   ]);
 
   return (
-    /*<p className="px-3 py-2 text-lg mt-5">
-      La session n&apos;est pas commencée, le temps restant est de{" "}
-      {`${totalDays.toFixed(0)} jours, ${remainingHours.toFixed(
-        0
-      )} heures, ${remainingMinutes.toFixed(0)} minutes, ${Math.round(
-        remainingSeconds
-      )} secondes ( ${dayjs(session.startTime).format("DD/MM/YYYY HH:mm")} )`}
-    </p>*/
     <div className="flex flex-col items-center justify-center w-full h-full gap-8 sm:gap-16">
-    <span className="text-2xl sm:text-3xl font-semibold text-white text-center tracking-widest px-2">
-      La session n’a pas encore commencé. Le temps restant est de
-    </span>
-    <div className="flex justify-center gap-3 sm:gap-8">
-      <div className="flex flex-col gap-5 relative">
-        <div className="h-16 w-16 sm:w-32 sm:h-32 lg:w-40 lg:h-40 flex justify-between items-center bg-quiz-theme  rounded-lg">
-          <div className="relative h-2.5 w-2.5 sm:h-3 sm:w-3 !-left-[6px] rounded-full bg-[#343A40ff]"></div>
-          <span className="lg:text-7xl sm:text-6xl text-3xl font-semibold text-white">
-            {totalDays.toFixed(0)}
+      <span className="text-2xl sm:text-3xl font-semibold text-white text-center tracking-widest px-2">
+        La session n’a pas encore commencé. Le temps restant est de
+      </span>
+      <div className="flex justify-center gap-3 sm:gap-8">
+        <div className="flex flex-col gap-5 relative">
+          <div className="h-16 w-16 sm:w-32 sm:h-32 lg:w-40 lg:h-40 flex justify-between items-center bg-quiz-theme  rounded-lg">
+            <div className="relative h-2.5 w-2.5 sm:h-3 sm:w-3 !-left-[6px] rounded-full bg-[#343A40ff]"></div>
+            <span className="lg:text-7xl sm:text-6xl text-3xl font-semibold text-white">
+              {totalDays.toFixed(0).padStart(2, '0')}
+            </span>
+            <div className="relative h-2.5 w-2.5 sm:h-3 sm:w-3 -right-[6px] rounded-full bg-[#343A40ff]"></div>
+          </div>
+          <span className="text-white text-xs sm:text-2xl text-center capitalize">
+            {totalDays.toFixed(0) == 1 ? "Jour" : "Jours"}
           </span>
-          <div className="relative h-2.5 w-2.5 sm:h-3 sm:w-3 -right-[6px] rounded-full bg-[#343A40ff]"></div>
         </div>
-        <span className="text-white text-xs sm:text-2xl text-center capitalize">
-          {totalDays.toFixed(0) == 1 ? "Jour" : "Jours"}
-        </span>
-      </div>
-      <div className="flex flex-col gap-5 relative">
-        <div className="h-16 w-16 sm:w-32 sm:h-32 lg:w-40 lg:h-40 flex justify-between items-center bg-quiz-theme  rounded-lg">
-          <div className="relative h-2.5 w-2.5 sm:h-3 sm:w-3 !-left-[6px] rounded-full bg-[#343A40ff]"></div>
-          <span className="lg:text-7xl sm:text-6xl text-3xl font-semibold text-white">
-            {remainingHours.toFixed(0)}
+        <div className="flex flex-col gap-5 relative">
+          <div className="h-16 w-16 sm:w-32 sm:h-32 lg:w-40 lg:h-40 flex justify-between items-center bg-quiz-theme  rounded-lg">
+            <div className="relative h-2.5 w-2.5 sm:h-3 sm:w-3 !-left-[6px] rounded-full bg-[#343A40ff]"></div>
+            <span className="lg:text-7xl sm:text-6xl text-3xl font-semibold text-white">
+              {remainingHours.toFixed(0).padStart(2, '0')}
+            </span>
+            <div className="relative h-2.5 w-2.5 sm:h-3 sm:w-3 -right-[6px] rounded-full bg-[#343A40ff]"></div>
+          </div>
+          <span className="text-white text-xs sm:text-2xl text-center font-medium">
+            {remainingHours.toFixed(0) == 1 ? "Heur" : "Heures"}
           </span>
-          <div className="relative h-2.5 w-2.5 sm:h-3 sm:w-3 -right-[6px] rounded-full bg-[#343A40ff]"></div>
         </div>
-        <span className="text-white text-xs sm:text-2xl text-center font-medium">
-          {remainingHours.toFixed(0) == 1 ? "Heur" : "Heures"}
-        </span>
-      </div>
-      <div className="flex flex-col gap-5 relative">
-        <div className="h-16 w-16 sm:w-32 sm:h-32 lg:w-40 lg:h-40 flex justify-between items-center bg-quiz-theme  rounded-lg">
-          <div className="relative h-2.5 w-2.5 sm:h-3 sm:w-3 !-left-[6px] rounded-full bg-[#343A40ff]"></div>
-          <span className="lg:text-7xl sm:text-6xl text-3xl font-semibold text-white">
-            {remainingMinutes.toFixed(0)}
+        <div className="flex flex-col gap-5 relative">
+          <div className="h-16 w-16 sm:w-32 sm:h-32 lg:w-40 lg:h-40 flex justify-between items-center bg-quiz-theme  rounded-lg">
+            <div className="relative h-2.5 w-2.5 sm:h-3 sm:w-3 !-left-[6px] rounded-full bg-[#343A40ff]"></div>
+            <span className="lg:text-7xl sm:text-6xl text-3xl font-semibold text-white">
+              {remainingMinutes.toFixed(0).padStart(2, '0')}
+            </span>
+            <div className="relative h-2.5 w-2.5 sm:h-3 sm:w-3 -right-[6px] rounded-full bg-[#343A40ff]"></div>
+          </div>
+          <span className="text-white text-xs sm:text-2xl text-center capitalize">
+            {remainingMinutes.toFixed(0) == 1 ? "Minute" : "Minutes"}
           </span>
-          <div className="relative h-2.5 w-2.5 sm:h-3 sm:w-3 -right-[6px] rounded-full bg-[#343A40ff]"></div>
         </div>
-        <span className="text-white text-xs sm:text-2xl text-center capitalize">
-          {remainingMinutes.toFixed(0) == 1 ? "Minute" : "Minutes"}
-        </span>
-      </div>
-      <div className="flex flex-col gap-5 relative">
-        <div className="h-16 w-16 sm:w-32 sm:h-32 lg:w-40 lg:h-40 flex justify-between items-center bg-quiz-theme  rounded-lg">
-          <div className="relative h-2.5 w-2.5 sm:h-3 sm:w-3 !-left-[6px] rounded-full bg-[#343A40ff]"></div>
-          <span className="lg:text-7xl sm:text-6xl text-3xl font-semibold text-white">
-            {Math.round(remainingSeconds)}
+        <div className="flex flex-col gap-5 relative">
+          <div className="h-16 w-16 sm:w-32 sm:h-32 lg:w-40 lg:h-40 flex justify-between items-center bg-quiz-theme  rounded-lg">
+            <div className="relative h-2.5 w-2.5 sm:h-3 sm:w-3 !-left-[6px] rounded-full bg-[#343A40ff]"></div>
+            <span className="lg:text-7xl sm:text-6xl text-3xl font-semibold text-white">
+              {Math.round(remainingSeconds).toString().padStart(2, '0')}
+            </span>
+            <div className="relative h-2.5 w-2.5 sm:h-3 sm:w-3 -right-[6px] rounded-full bg-[#343A40ff]"></div>
+          </div>
+          <span className="text-white text-xs sm:text-2xl text-center capitalize">
+            {Math.round(remainingSeconds) == 1 ? "Second" : "Secondes"}
           </span>
-          <div className="relative h-2.5 w-2.5 sm:h-3 sm:w-3 -right-[6px] rounded-full bg-[#343A40ff]"></div>
         </div>
-        <span className="text-white text-xs sm:text-2xl text-center capitalize">
-          {Math.round(remainingSeconds) == 1 ? "Second" : "Secondes"}
-        </span>
       </div>
     </div>
-  </div>
   );
 }
